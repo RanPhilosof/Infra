b64a83a236a1837bf2ab6d439861adb26307b78a
Ran Philosof <ranphilosof@gmail.com>
2025-04-26 22:19:43 +0300

    Create Logger Interface + Static Converter From Guid To Int128 And Vice Versa

---
+++ RP.Infra/ILogger.cs
@@ -0,0 +1,10 @@
+﻿namespace RP.Infra.Logger
+{
+    public interface ILogger
+    {
+        void Error(Exception ex);
+        void Error(string message);
+        void Warning(string message);
+        void Info(string message);
+    }
+}
---
+++ RP.Infra/TypeConverters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RP.Infra.TypeConverters
+{
+    public static class GuidInt128Converter
+    {
+        public static Int128 GuidToInt128(Guid guid)
+        {
+            Span<byte> guidBytes = stackalloc byte[16];
+            guid.TryWriteBytes(guidBytes);
+            return MemoryMarshal.Read<Int128>(guidBytes);
+        }
+
+        public static Guid Int128ToGuid(Int128 value)
+        {
+            Span<byte> int128Bytes = stackalloc byte[16];
+            MemoryMarshal.Write(int128Bytes, ref value);
+
+            return new Guid(int128Bytes);
+        }
+    }
+}
